6d301c21f1b5cfff61120053d68fe476b4fd941c
Mat <mathurin.malandain+github@gmail.com>
2024-06-07 16:34:27 +0200

    V2

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -6,10 +6,11 @@ function App() {
   return (
     <div id="container">
         <header>
-          <h1 className="title">La Roue de la Deut-une</h1>
+          <h1 className="title">La Roue de la Deut-une de Bharani</h1>
           <h2 className="subtitle">- offerte par la DDN -</h2>
-          <p className="intro">Un tirage toutes les 48h, l'alliance gagnante aura l'honneur de livrer 1G deut à la DDN, sous peine de représailles.</p>
-          <p className="intro">Vous ne souhaitez pas être tiré au sort ? Contactez la DDN et réglez 250M deut pour être exclu du prochain tirage !</p>
+          <p>Un tirage chaque semaine ! Un tirage consiste en deux lancers de la Roue de la Deut-une. La première alliance tirée recevra un pull de <strong><u>500M deut dans les 48h</u></strong> de la part de la seconde alliance (ou commerce à taux avantageux de 1G deut si pull impossible).</p>
+          <p>Vous ne souhaitez pas être sur la Roue de la Deut-une ? Contactez la DDN et réglez <strong><u>30M</u></strong> deut pour être exclu du prochain tirage !</p>
+          <p className="disclaimer">Toute infraction aux règles de la Roue de la Deut-une sera sévèrement réprimée par la DDN.</p>
         </header>
         <main>
             <Wheel/>
