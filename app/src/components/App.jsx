8aeaab2887257f2954a6a97985535b1681e7ac90
Nrirvadoth <mathurin.malandain+github@gmail.com>
2025-12-31 15:55:08 +0100

    maj texte

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -6,10 +6,10 @@ function App() {
   return (
     <div id="container">
         <header>
-          <h1 className="title">La Roue de la fortune</h1>
-          <h2 className="subtitle">- offerte par la DDN -</h2>
-{/*           <p>Les règles de la Roue de la Deut-une sont variables et édictées par la DDN - elles peuvent être modifiées à tout instant.</p>
-          <p className="disclaimer">Toute infraction aux règles de la Roue de la Deut-une sera sévèrement réprimée par la DDN.</p> */}
+          <h1 className="title">La Roue de l'infortune NGE</h1>
+          <h2 className="subtitle">- offerte par la DDN, en collaboration avec GAT et Kay -</h2>
+         <p>Afin d'offrir à l'alliance NGE un beau cadeau en ce début d'année, la roue désignera un NGE de façon régulière afin qu'il soit le centre de l'attention sur Tucana. Bien évidemment, en cas de MV, il faudrait désigner un nouveau NGE.</p>
+         {/* <p className="disclaimer">Toute infraction aux règles de la Roue de la Deut-une sera sévèrement réprimée par la DDN.</p> */}
         </header>
         <main>
             <Wheel/>
